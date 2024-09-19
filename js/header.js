d1ff0d44e299402ef10b9af853b8d7ed8eb4cd67
Abraham Samuel <ebubesammy567@gmail.com>
2024-09-19 07:43:50 +0100

    commit

--- js/header.js
+++ js/header.js
@@ -1,8 +1,12 @@
-function openNav(){
-    document.getElementById("myNav").style.width = "100%"; 
-    document.getElementsByClassName("nav-menu").style.right = "0%"; 
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+  document.getElementById("myNav").style.boxShadow =
+    "-1px 0.7px 4px 3px var(--light-purple-color)";
+
+  // Target the first element with class 'nav-menu'
+  document.getElementsByClassName("nav-menu")[0].style.right = "-0%";
 }
 
-function closeNav(){
-    document.getElementById("myNav").style.width = "0%";
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
 }
